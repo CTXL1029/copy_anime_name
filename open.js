bdcc1dfecc0a11635039e3950af3d8b20b960b0f
CTXL1029 <110585319+CTXL1029@users.noreply.github.com>
2024-09-03 21:51:25 +0700

    fix litle bug

--- open.js
+++ open.js
@@ -1,27 +1,27 @@
 function open_1() {
-    window.open("run/copyText_1.html");
+    window.open("run/copyText_1", "_blank");
 }
 
 function open_2() {
-    window.open("run/copyText_2.html");
+    window.open("run/copyText_2", "_blank");
 }
 
 function open_3() {
-    window.open("run/copyText_3.html");
+    window.open("run/copyText_3", "_blank");
 }
 
 function open_4() {
-    window.open("run/copyText_4.html");
+    window.open("run/copyText_4", "_blank");
 }
 
 function open_5() {
-    window.open("run/copyText_5.html");
+    window.open("run/copyText_5", "_blank");
 }
 
 function open_6() {
-    window.open("run/copyText_6.html");
+    window.open("run/copyText_6", "_blank");
 }
 
 function open_7() {
-    window.open("run/copyText_7.html");
+    window.open("run/copyText_7", "_blank");
 }
