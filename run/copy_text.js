7c8307472277b3835dba99f6a9c6c8fd9b369c62
CTXL1029 <110585319+CTXL1029@users.noreply.github.com>
2024-09-03 21:32:41 +0700

    1st change

---
+++ run/copy_text.js
@@ -0,0 +1,78 @@
+/* Copy text into clipboard */
+
+function copyText_1()
+{   
+    const name_1 = "Nhật ký sống chậm nơi dị giới - Nuôi con trong lúc làm mạo hiểm giả";
+    navigator.clipboard.writeText(name_1);
+    alert("Đã sao chép tên bộ Anime:\n"+ name_1);
+    setTimeout(() => {
+        close();
+        button.disabled = false;
+    }, 2000);
+}
+
+function copyText_2()
+{   
+    const name_2 = "Mayonaka Punch";
+    navigator.clipboard.writeText(name_2)
+    alert("Đã sao chép tên bộ Anime:\n"+ name_2);
+    setTimeout(() => {
+        close();
+        button.disabled = false;
+    }, 2000);
+}
+
+function copyText_3()
+{   
+    const name_3 = "Arya Bàn Bên Thỉnh Thoảng Lại Trêu Ghẹo Tôi Bằng Tiếng Nga";
+    navigator.clipboard.writeText(name_3);
+    alert("Đã sao chép tên bộ Anime:\n"+ name_3);
+    setTimeout(() => {
+        close();
+        button.disabled = false;
+    }, 2000);
+}
+
+function copyText_4()
+{
+    const name_4 = "[Oshi no Ko] 2nd Seasons";
+    navigator.clipboard.writeText(name_4);
+    alert("Đã sao chép tên bộ Anime:\n"+ name_4);
+    setTimeout(() => {
+        close();
+        button.disabled = false;
+    }, 2000);
+}
+
+function copyText_5()
+{
+    const name_5 = "Koi wa Futago de Warikirenai";
+    navigator.clipboard.writeText(name_5);
+    alert("Đã sao chép tên bộ Anime:\n"+ name_5);
+    setTimeout(() => {
+        close();
+        button.disabled = false;
+    }, 2000);
+}
+
+function copyText_6()
+{
+    const name_6 = "Shikanoko Nokonoko Koshitantan";
+    navigator.clipboard.writeText(name_6);
+    alert("Đã sao chép tên bộ Anime:\n"+ name_6);
+    setTimeout(() => {
+        close();
+        button.disabled = false;
+    }, 2000);
+}
+
+function copyText_7()
+{
+    const name_7 = "Hậu Cung Giả Tạo";
+    navigator.clipboard.writeText(name_7);
+    alert("Đã sao chép tên bộ Anime:\n"+ name_7);
+    setTimeout(() => {
+        close();
+        button.disabled = false;
+    }, 2000);
+}
