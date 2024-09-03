0d2e4a2b0e5b852075d817005610a66c84bc9dc4
CTXL1029 <110585319+CTXL1029@users.noreply.github.com>
2024-09-03 21:53:08 +0700

    missing ".html"  are fixed

--- open.js
+++ open.js
@@ -1,27 +1,27 @@
 function open_1() {
-    window.open("run/copyText_1", "_blank");
+    window.open("run/copyText_1.html", "_blank");
 }
 
 function open_2() {
-    window.open("run/copyText_2", "_blank");
+    window.open("run/copyText_2.html", "_blank");
 }
 
 function open_3() {
-    window.open("run/copyText_3", "_blank");
+    window.open("run/copyText_3.html", "_blank");
 }
 
 function open_4() {
-    window.open("run/copyText_4", "_blank");
+    window.open("run/copyText_4.html", "_blank");
 }
 
 function open_5() {
-    window.open("run/copyText_5", "_blank");
+    window.open("run/copyText_5.html", "_blank");
 }
 
 function open_6() {
-    window.open("run/copyText_6", "_blank");
+    window.open("run/copyText_6.html", "_blank");
 }
 
 function open_7() {
-    window.open("run/copyText_7", "_blank");
+    window.open("run/copyText_7.html", "_blank");
 }
