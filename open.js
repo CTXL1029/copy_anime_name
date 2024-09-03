3bfb3e50841d455dfb7699bd3a16f4846057ca2a
CTXL1029 <110585319+CTXL1029@users.noreply.github.com>
2024-09-03 22:23:45 +0700

    fix some mistakes

--- open.js
+++ open.js
@@ -1,41 +1,41 @@
 function open_1() {
-    const opn_1 = "run/copyText_1.html";
+    const opn_1 = "https://ctxl1029.github.io/copy_anime_name/run/copyText_1.html";
     navigator.clipboard.writeText(opn_1);
     window.open(opn_1, "_blank");
 }
 
 function open_2() {
-    const opn_2 = "run/copyText_1.html";
+    const opn_2 = "https://ctxl1029.github.io/copy_anime_name/run/copyText_2.html";
     navigator.clipboard.writeText(opn_2);
     window.open(opn_2, "_blank");
 }
 
 function open_3() {
-    const opn_3 = "run/copyText_1.html";
+    const opn_3 = "https://ctxl1029.github.io/copy_anime_name/run/copyText_3.html";
     navigator.clipboard.writeText(opn_3);
     window.open(opn_3, "_blank");
 }
 
 function open_4() {
-    const opn_4 = "run/copyText_1.html";
+    const opn_4 = "https://ctxl1029.github.io/copy_anime_name/run/copyText_4.html";
     navigator.clipboard.writeText(opn_4);
     window.open(opn_4, "_blank");
 }
 
 function open_5() {
-    const opn_5 = "run/copyText_1.html";
+    const opn_5 = "https://ctxl1029.github.io/copy_anime_name/run/copyText_5.html";
     navigator.clipboard.writeText(opn_5);
     window.open(opn_5, "_blank");
 }
 
 function open_6() {
-    const opn_6 = "run/copyText_1.html";
+    const opn_6 = "https://ctxl1029.github.io/copy_anime_name/run/copyText_6.html";
     navigator.clipboard.writeText(opn_6);
     window.open(opn_6, "_blank");
 }
 
 function open_7() {
-    const opn_7 = "run/copyText_1.html";
+    const opn_7 = "https://ctxl1029.github.io/copy_anime_name/run/copyText_7.html";
     navigator.clipboard.writeText(opn_7);
     window.open(opn_7, "_blank");
 }
